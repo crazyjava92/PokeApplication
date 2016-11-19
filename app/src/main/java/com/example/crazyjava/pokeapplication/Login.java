39ca2f770eda284c1df3838dd3df5f960b1d6dec
Miguel Angel <miguelm37@gmail.com>
2016-11-19 18:27:25 +0100

    Login sin BBDD.

--- app/src/main/java/com/example/crazyjava/pokeapplication/Login.java
+++ app/src/main/java/com/example/crazyjava/pokeapplication/Login.java
@@ -2,12 +2,46 @@ package com.example.crazyjava.pokeapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    Button botonEntrar;
+    EditText txtusuario, txtpassword;
+    TextView lblLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txtusuario = (EditText)findViewById(R.id.txtUsuario);
+        txtpassword = (EditText)findViewById(R.id.txtpassword);
+        lblLogin = (TextView)findViewById(R.id.lblErrorLogin);
+        botonEntrar = (Button)findViewById(R.id.btnEntrar);
+
+        botonEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = txtusuario.getText().toString();
+                String pass = txtpassword.getText().toString();
+
+                if(user.equals("a") && pass.equals("aa")){
+
+                    lblLogin.setText("Usuario OK");
+
+                }
+                else {
+                    lblLogin.setText("Usuario o contraseña incorrecto");
+                }
+
+            }
+        });
+
+
+
     }
 }
